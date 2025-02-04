7b2b548d20427b1c06952fb7b3dbe10bc93f3556
Ravikiran Patil <Ravip@powercattools.onmicrosoft.com>
2025-02-05 00:10:00 +0530

    End user auth changes

--- POWERCAT/POWERCAT/POWERCAT.Webresources/CopilotConfiguration/copilotconfiguration_main.js
+++ POWERCAT/POWERCAT/POWERCAT.Webresources/CopilotConfiguration/copilotconfiguration_main.js
@@ -102,29 +102,6 @@ function setFieldVisibilityForEachSections(executionContext) {
   const configurationTypeValues =
     formContext.getAttribute("cat_configurationtypescodes").getValue() || [];
 
-  // User Authentication Fields Rules
-  const userAuth = formContext
-    .getAttribute("cat_userauthenticationcode")
-    .getValue();
-
-  // Entra ID v2 and Test Automation
-  if (userAuth === 2 && configurationTypeValues.includes(1)) {
-    setFieldVisibility(
-      formContext,
-      ["cat_clientid", "cat_tenantid", "cat_scope"],
-      true,
-      "required"
-    );
-
-    // No Authentication and Test Automation
-  } else if (userAuth === 1 && configurationTypeValues.includes(1)) {
-    clearAndHideFields(formContext, [
-      "cat_clientid",
-      "cat_tenantid",
-      "cat_scope",
-    ]);
-  }
-
   // Enrich With Azure Application Insights Secret Location Rule
   const secretLocation = formContext
     .getAttribute("cat_azureappinsightssecretlocationcode")
